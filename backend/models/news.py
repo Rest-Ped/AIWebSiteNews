738e3fb6270ee010249e42c2f4fa92ea7bb7e1f2
Rest-Pred <144260777+Rest-Ped@users.noreply.github.com>
2026-05-13 20:55:55 +0700

    Delete backend directory

--- backend/models/news.py
+++
@@ -1,2 +0,0 @@
-# Placeholder for news model extensions
-pass
--- backend/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Сервисы приложения"""
-from .assistant_service import AssistantService
-from .llm_service import LLMService
-from .news_fetcher import NewsFetcher
-from .summarizer import Summarizer
-
-__all__ = ['AssistantService', 'LLMService', 'NewsFetcher', 'Summarizer']
